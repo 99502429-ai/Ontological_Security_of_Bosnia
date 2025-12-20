d2a2db06ba9ae103e1fb1f1c82accce84ccc9dce
99502429-ai <99502429@fsv.cuni.cz>
2025-12-20 20:35:51 +0100

    Fix Packages

--- Scripts/Bosnia_Crawler.py
+++ Scripts/Bosnia_Crawler.py
@@ -1,10 +1,10 @@
-# import pandas as pd
-# import numpy as np
-# import requests
-# import json
-# import re
-# from bs4 import BeautifulSoup
-# from newsplease import NewsPlease
+import pandas as pd
+import numpy as np
+import requests
+import json
+import re
+from bs4 import BeautifulSoup
+from newsplease import NewsPlease
 # from easynmt import EasyNMT
 # from torch import cuda
 # import nltk
